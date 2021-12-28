8cc4b17fa87d31888bb2494d7f34396467af271c
bluegleam0101 <sennasoeditos2@gmail.com>
2021-12-28 14:16:26 +0100

    second commit

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sql    ite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-
+#########################
 
 @login_manager.user_loader
 def load_user(user_id):
